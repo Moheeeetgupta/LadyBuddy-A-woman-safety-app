4f8bf7740e71e5c26a7b48819938dedee70e8c0e
Shivani <shivanikumari6109@gmail.com>
2021-08-19 00:43:47 +0530

    Device Detection features updated

--- app/src/main/java/com/moheeeetgupta/ladybuddy/Magnetometer.java
+++ app/src/main/java/com/moheeeetgupta/ladybuddy/Magnetometer.java
@@ -20,8 +20,8 @@ public class Magnetometer extends AppCompatActivity implements SensorEventListen
 
     private TextView magR ;
 
-    MediaPlayer mediaPlayer1;
-    MediaPlayer mediaPlayer2;
+    MediaPlayer mediaPlayer;
+    //MediaPlayer mediaPlayer2
 
 
     private double magD;
@@ -62,48 +62,29 @@ public class Magnetometer extends AppCompatActivity implements SensorEventListen
 
 
         if (sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD){
-
-
-
-
             double x;
-
             x = sqrt (event.values[0]*event.values[0]+event.values[1]*event.values[1]+event.values[2]*event.values[2]);
-            magR.setText(Double.toString(x));
-
-
-            mediaPlayer1 = MediaPlayer.create(getApplicationContext(), R.raw.beep);
-            mediaPlayer2 = MediaPlayer.create(getApplicationContext(), R.raw.beepd);
-
-
-            if(x>70.0 && x< 90.0) {
-                if(mediaPlayer2.isPlaying()){
-                    //mediaPlayer2.setLooping(false);
-                    mediaPlayer2.pause();
-                }
-                mediaPlayer1.start();
-                mediaPlayer1.setLooping(true);
-                Toast.makeText(getApplicationContext(), "Potential electronic device detected", Toast.LENGTH_SHORT).show();
-
-            }else if(x>90.0){
-
-                mediaPlayer1.setLooping(false);
-                mediaPlayer1.stop();
-                mediaPlayer2.start();
-                mediaPlayer2.setLooping(true);
-                Toast.makeText(getApplicationContext(), "Finally electronic device detected", Toast.LENGTH_SHORT).show();
+            magR.setText(String.format("%.2f", x));
+            double Pd=70d;
+            double Fd=90d;
+            if(Double.compare (x,Pd)>0 && Double.compare (x,Fd)<0) {
+                mediaPlayer=null;
+                mediaPlayer = MediaPlayer.create(this, R.raw.beep);
+                mediaPlayer.start();
+                Toast.makeText(this, "Potential electronic device detected", Toast.LENGTH_SHORT).show();
 
             }
+            else if(Double.compare (x,Fd)>0){
+                mediaPlayer=null;
+                mediaPlayer = MediaPlayer.create(this, R.raw.beepd);
+                mediaPlayer.start();
+                Toast.makeText(this, "Finally electronic device detected", Toast.LENGTH_SHORT).show();
 
-            else{
-                mediaPlayer1.setLooping(false);
-                mediaPlayer1.stop();
-                mediaPlayer2.setLooping(false);
-                mediaPlayer2.stop();
+            }else {
+                mediaPlayer=null;
             }
 
         }
-
 
 
     }
@@ -116,44 +97,7 @@ public class Magnetometer extends AppCompatActivity implements SensorEventListen
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
-    }
-
-
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        releaseMediaPlayer();
-    }
-    /**
-     * Clean up the media player by releasing its resources.
-     */
-    private void releaseMediaPlayer() {
-        // If the media player is not null, then it may be currently playing a sound.
-        if (mediaPlayer1 != null) {
-            // Regardless of the current state of the media player, release its resources
-            // because we no longer need it.
-            mediaPlayer1.release();
-
-            // Set the media player back to null. For our code, we've decided that
-            // setting the media player to null is an easy way to tell that the media player
-            // is not configured to play an audio file at the moment.
-            mediaPlayer1 = null;
-
-
-            if (mediaPlayer2 != null) {
-                // Regardless of the current state of the media player, release its resources
-                // because we no longer need it.
-                mediaPlayer2.release();
-
-                // Set the media player back to null. For our code, we've decided that
-                // setting the media player to null is an easy way to tell that the media player
-                // is not configured to play an audio file at the moment.
-                mediaPlayer2 = null;
-
-
-            }
-        }
-
-
     }}
+
+
+
