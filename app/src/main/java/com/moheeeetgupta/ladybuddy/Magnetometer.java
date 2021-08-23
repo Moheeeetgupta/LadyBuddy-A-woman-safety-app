0cef7c99e11392b94609e94da07b01535ff9ca49
sakshi gupta <sakshigupta82401@gmail.com>
2021-08-23 22:11:17 +0530

    Magnetometer UI improved

--- app/src/main/java/com/moheeeetgupta/ladybuddy/Magnetometer.java
+++ app/src/main/java/com/moheeeetgupta/ladybuddy/Magnetometer.java
@@ -1,6 +1,7 @@
 package com.moheeeetgupta.ladybuddy;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -15,10 +16,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import static java.lang.Math.sqrt;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Magnetometer extends AppCompatActivity implements SensorEventListener {
 
 
-    private TextView magR ;
+    private TextView magR, show_conditions, x_cor, y_cor, z_cor ;
+    SpeedometerView Speed;
 
     MediaPlayer mediaPlayer1;
     MediaPlayer mediaPlayer2;
@@ -29,6 +34,8 @@ public class Magnetometer extends AppCompatActivity implements SensorEventListen
     private Sensor magnetometer ;
 
     private SensorManager sensorManager;
+    Boolean flag = false;
+    double prevx = 0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +43,38 @@ public class Magnetometer extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_magnetometer);
 
 
+        Speed = (SpeedometerView)findViewById(R.id.speedometer);
+        Speed.setLabelConverter(new SpeedometerView.LabelConverter() {
+            @Override
+            public String getLabelFor(double progress, double maxProgress) {
+                return String.valueOf((int) Math.round(progress));
+            }
+        });
+
+        // configure value range and ticks
+        Speed.setMaxSpeed(100);
+        Speed.setMajorTickStep(10);
+        Speed.setMinorTicks(0);
+
+        // Configure value range colors
+        Speed.addColoredRange(0, 10, Color.GREEN);
+        Speed.addColoredRange(10, 20, Color.YELLOW);
+        Speed.addColoredRange(20, 30, Color.RED);
+        Speed.addColoredRange(30, 40, Color.GREEN);
+        Speed.addColoredRange(40, 50, Color.YELLOW);
+        Speed.addColoredRange(50, 60, Color.RED);
+        Speed.addColoredRange(60, 70, Color.GREEN);
+        Speed.addColoredRange(70, 80, Color.YELLOW);
+        Speed.addColoredRange(80, 90, Color.RED);
+        Speed.addColoredRange(90, 100, Color.GREEN);
 
 
-        magR = (TextView) findViewById(R.id.textView4);
 
-
+        magR = (TextView) findViewById(R.id.value);
+        show_conditions = findViewById( R.id.show_conditions );
+        x_cor = findViewById( R.id.x_cor );
+        y_cor = findViewById( R.id.y_cor );
+        z_cor = findViewById( R.id.z_cor );
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
 
@@ -68,30 +102,77 @@ public class Magnetometer extends AppCompatActivity implements SensorEventListen
 
             double x;
 
-            x = sqrt (event.values[0]*event.values[0]+event.values[1]*event.values[1]+event.values[2]*event.values[2]);
-            magR.setText(Double.toString(x));
 
+
+
+            x = sqrt (event.values[0]*event.values[0]+event.values[1]*event.values[1]+event.values[2]*event.values[2]);
+
+
+            BigDecimal bd = new BigDecimal(x).setScale(0, RoundingMode.HALF_UP);
+            double newx = bd.doubleValue();
+
+
+
+            if(newx>0 && newx<100){
+                /*
+
+                if(x < prevx && prevx != 0){
+                    flag = true;
+                }else{
+                    flag = false;
+                }
+                 */
+                //  if(flag == false){
+                Speed.setSpeed(newx, 1, 1);
+                //  }
+
+                // prevx = x;
+
+
+            }
+            magR.setText(Double.toString(newx) + " μT");
+            //x_cor.setText(String.format( "%0f", event.values[0]) );
+
+            BigDecimal bdx = new BigDecimal(event.values[0]).setScale(0, RoundingMode.HALF_UP);
+            double new_x = bdx.doubleValue();
+            x_cor.setText(Double.toString(new_x));
+            x_cor.setBackgroundColor( Color.GREEN );
+
+            BigDecimal bdy = new BigDecimal(event.values[1]).setScale(0, RoundingMode.HALF_UP);
+            double new_y = bdy.doubleValue();
+            y_cor.setText(Double.toString(new_y));
+            y_cor.setBackgroundColor( Color.RED );
+
+
+            BigDecimal bdz = new BigDecimal(event.values[2]).setScale(0, RoundingMode.HALF_UP);
+            double new_z = bdz.doubleValue();
+            z_cor.setText(Double.toString(new_z));
+            z_cor.setBackgroundColor( Color.YELLOW );
 
             mediaPlayer1 = MediaPlayer.create(getApplicationContext(), R.raw.beep);
             mediaPlayer2 = MediaPlayer.create(getApplicationContext(), R.raw.beepd);
 
 
-            if(x>70.0 && x< 90.0) {
+
+
+            if(newx>70.0 && newx< 90.0) {
                 if(mediaPlayer2.isPlaying()){
                     //mediaPlayer2.setLooping(false);
                     mediaPlayer2.pause();
                 }
                 mediaPlayer1.start();
                 mediaPlayer1.setLooping(true);
-                Toast.makeText(getApplicationContext(), "Potential electronic device detected", Toast.LENGTH_SHORT).show();
+                //  Toast.makeText(getApplicationContext(), "Potential electronic device detected", Toast.LENGTH_SHORT).show();
+                show_conditions.setText( "Potential electronic device detected" );
 
-            }else if(x>90.0){
+            }else if(newx>90.0){
 
                 mediaPlayer1.setLooping(false);
                 mediaPlayer1.stop();
                 mediaPlayer2.start();
                 mediaPlayer2.setLooping(true);
-                Toast.makeText(getApplicationContext(), "Finally electronic device detected", Toast.LENGTH_SHORT).show();
+                // Toast.makeText(getApplicationContext(), "Finally electronic device detected", Toast.LENGTH_SHORT).show();
+                show_conditions.setText( "Finally electronic device detected." );
 
             }
 
@@ -100,6 +181,7 @@ public class Magnetometer extends AppCompatActivity implements SensorEventListen
                 mediaPlayer1.stop();
                 mediaPlayer2.setLooping(false);
                 mediaPlayer2.stop();
+                show_conditions.setText( "No Potential electronic device detected" );
             }
 
         }
